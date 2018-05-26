cb67d13b6af3c6af5e9a01b5359d48c0a684c851
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-26 00:08:57 -0400

    js listo, html vacío

---
+++ app.js
@@ -0,0 +1,20 @@
+cant = prompt('¿Cuántos platos hay disponibles en el menú?');
+i=0;
+arrPlatos = [];
+while (i < cant){
+    arrPlatos[i] = prompt('Ingrese plato disponible');
+    i++;
+}
+
+function chooseFood() {
+    if (arrPlatos.indexOf('ceviche') != -1) {
+        alert('Elijo ceviche');
+    } else if (arrPlatos.indexOf('tacos al pastor') != -1) {
+        alert('Elijo tacos al pastor');
+    } else if (arrPlatos.indexOf('empanada chilena') != -1) {
+        alert('Elijo empanada chilena');
+    } else {
+        alert('Sólo una hamburguesa');
+    }
+}
+console.log(chooseFood());
